6fba8daab141b460bcc96170463df3c96e1dc0f4
Jared Mullins <jwmullins92@gmail.com>
2021-06-24 21:08:52 -0500

    Deployed

--- middleware.js
+++ middleware.js
@@ -2,7 +2,10 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl
-        req.flash('error', 'You must be signed in');
+        if (req.originalUrl !== '/dashboard') {
+            req.flash('error', 'You must be signed in');
+            return res.redirect('/login')
+        }
         return res.redirect('/login')
     }
     next()
